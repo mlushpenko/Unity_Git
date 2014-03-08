6dd8e951367f810cb027da894f78946c84b5aad1
mlushpenko <iviakciivi@gmail.com>
2014-03-08 02:32:37 +0200

    Ball is moving now
    Used javascript instead of C# from the example

---
+++ Roll-a-Ball/Assets/Scripts/PlayerController.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+var speed:int = 0;
+function Update () {
+	var vertical:float = Input.GetAxis("Vertical");
+	var horizontal:float = Input.GetAxis("Horizontal");
+	
+	rigidbody.AddForce(horizontal*speed*Time.deltaTime, 0, vertical*speed*Time.deltaTime);
+
+}
