19b94c088e7fbebcf778bc50845db02c3aec81d1
mlushpenko <iviakciivi@gmail.com>
2014-03-09 03:21:22 +0200

    Finished roll-a-ball mini game
    Try downloading 2 files from Builds folder and open html page - you
    should be able to play game.

--- Roll-a-Ball/Assets/Scripts/PlayerController.js
+++ Roll-a-Ball/Assets/Scripts/PlayerController.js
@@ -1,13 +1,35 @@
 ﻿#pragma strict
 
-function Start () {
-
-}
 var speed:int = 0;
-function Update () {
+private var count:int;
+var countText:GUIText;
+var winText:GUIText;
+
+function Start () {
+	count = 0;
+	SetCountText();
+	winText.text = "";
+}
+
+function FixedUpdate () {
 	var vertical:float = Input.GetAxis("Vertical");
 	var horizontal:float = Input.GetAxis("Horizontal");
 	
 	rigidbody.AddForce(horizontal*speed*Time.deltaTime, 0, vertical*speed*Time.deltaTime);
 
+}
+
+function OnTriggerEnter (other:Collider) {
+	if(other.gameObject.tag == "PickUp"){
+		other.gameObject.SetActive(false);
+		count++;
+		SetCountText();
+	}
+}
+
+function SetCountText(){
+	countText.text = "Count: " + count;
+	if (count >= 12){
+		winText.text = "YOU WON!";
+	}
 }
---
+++ Roll-a-Ball/Assets/Scripts/Rotator.js
@@ -0,0 +1,6 @@
+﻿#pragma strict
+
+
+function Update () {
+	transform.Rotate(new Vector3(15,30,45)* Time.deltaTime);
+}
